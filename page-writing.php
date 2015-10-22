65f67ecdf626981340388763675680d8ed5f7c73
Brandon <brandon@tonys-iMac.local>
2015-10-21 19:47:11 -0700

    updated writing page

--- page-writing.php
+++ page-writing.php
@@ -9,21 +9,23 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // End of the loop. ?>
-
+		<div class="works-wrap">
+			<h2 class="section-title">writing.</h2>
+			<div class="work-section writing-section">
+				<?php $loop = new WP_Query( array( 'post_type' => 'writing' ) ); ?>
+				<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+					<?php $paper = get_field('written_piece'); ?>
+					<div class="paper-wrap">
+						<p class="paper-title"><?php echo the_title(); ?></p>
+						<div class="work paper">
+							<embed src="<?php echo $paper; ?>">
+						</div>
+						<a href="<?php echo $paper; ?>">
+							<p class="paper-button">Click here to view the PDF</p>
+						</a>
+					</div>
+				<?php endwhile; wp_reset_query(); ?>
+			</div>
+		</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php get_footer(); ?>
